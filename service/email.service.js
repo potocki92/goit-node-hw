2ce951c02dae06c8c2396b0a9865fb658c623aa4
potocki92 <mateusz.potocki92@gmail.com>
2023-09-14 12:04:00 +0200

    config
    * Zmieniłem config z .env

--- service/email.service.js
+++ service/email.service.js
@@ -3,19 +3,19 @@ const User = require("../models/user.model");
 require("dotenv").config();
 const config = {
   pool: true,
-  host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
+  host: "smtp-relay.brevo.com",
+  port: 587,
   secure: false,
   auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,
+    user: "mateusz.potocki92@gmail.com",
+    pass: "KsXwvh1qMkZBrWpb",
   },
 };
 
 const send = async (email, verificationToken) => {
   const transporter = nodemailer.createTransport(config);
   const emailOptions = {
-    from: process.env.MAIL_USER,
+    from: "mateusz.potocki92@gmail.com",
     to: email,
     subject: "Email Verification",
     text: `Please click the link below to verify your email:<br>http://localhost:3000/api/users/verify/${verificationToken}`,
